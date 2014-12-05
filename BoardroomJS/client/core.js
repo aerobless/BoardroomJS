1e62392dd6ed5c93ee489f321f032f31caf215b9
TheoWinter <theo.winter@gmail.com>
2014-12-05 17:12:08 +0100

    finally got the godamn drawing working... apperently I have to create a new point instead of using the point I send across the server..

--- BoardroomJS/client/core.js
+++ BoardroomJS/client/core.js
@@ -4,16 +4,5 @@
 
 window.onload = function () {
     "use strict";
-    //Setup socket.io
-    var socket = io();
-    socket.on('message', function (msg) {
-        if (msg.action === "AddLink") {
-            showAlert("A new link has been published.");
-        } else if (msg.action === "Rated") {
-            showAlert("A link has been rated.");
-        } else if (msg.action === "AddComment"){
-            showAlert("A comment has been added.");
-        }
-    });
 
 };
--- BoardroomJS/client/papercraft.js
+++ BoardroomJS/client/papercraft.js
@@ -5,28 +5,25 @@ var path;
 var data; //debugging / test
 var socket = io();
 
-var textItem = new PointText(new Point(20, 30));
-textItem.fillColor = 'black';
-textItem.content = 'Click and drag to draw a line.';
-
 function onMouseDown(event) {
     socket.emit("mouseDown");
     mouseDown();
 }
 function mouseDown() {
     // If we produced a path before, deselect it:
-    if (path) {
+    /*if (path) {
         path.selected = false;
-    }
+    }*/
 
     path = new Path();
     path.strokeColor = 'black';
 
     // Select the path, so we can see its segment points:
-    path.fullySelected = true;
+   // path.fullySelected = true;
 }
 
 function onMouseDrag(event) {
+    console.log(event);
     mouseDrag(event.point);
     socket.emit("mouseDrag", event.point);
 }
@@ -35,49 +32,49 @@ function mouseDrag(eventPoint) {
     // Every drag event, add a point to the path at the current
     // position of the mouse:
     path.add(eventPoint);
-    textItem.content = 'Segment count: ' + path.segments.length;
+    console.log(path.index);
 }
 
 function onMouseUp(event) {
-    socket.emit("mouseUp");
-    mouseUp();
+    //socket.emit("mouseUp");
+   // mouseUp();
 }
 
 function mouseUp(event){
-    var segmentCount = path.segments.length;
-
     // When the mouse is released, simplify it:
-    path.simplify();
+   // path.simplify();
 
     // Select the path, so we can see its segments:
-    path.selected = true;
+    //path.selected = true;
 
-    var newSegmentCount = path.segments.length;
-    var difference = segmentCount - newSegmentCount;
-    var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-    textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+    //console.log(project.activeLayer);
 }
 
 document.getElementById("saveButton").onclick = function () {
-    /*alert("save in progress");
-    var data = path.exportJSON();
-    download("test.txt", data);
-    return false;*/
-    data = path.exportJSON();
-    project.activeLayer.remove();
+   // alert("save in progress");
+    //var data = path.exportJSON();
+    //download("test.txt", data);
+   // return false;
+   // data = path.exportJSON();
+   // project.activeLayer.remove();
     //var layer = new Layer();
-};
 
+    var circlePath = new Path.Circle(new Point(50, 50), 25);
+    circlePath.strokeColor = 'black';
+
+};
+/*
 document.getElementById("loadButton").onclick = function () {
-    /*alert("save in progress");
+    alert("save in progress");
      var data = path.exportJSON();
      download("test.txt", data);
-     return false;*/
+     return false;
     path.importJSON(data);
     var group = new Group();
     group.addChild(path);
     //var layer = new Layer();
 };
+*/
 
 function download(filename, text) {
     var pom = document.createElement('a');
@@ -93,7 +90,8 @@ socket.on('mouseDown', function (msg) {
 
 socket.on('mouseDrag', function (msg) {
     console.log("mouseDrag");
-    mouseDrag(msg);
+    mouseDrag(new Point( msg[1], msg[2] ));
+    view.draw();
 });
 
 socket.on('mouseUp', function (msg) {
