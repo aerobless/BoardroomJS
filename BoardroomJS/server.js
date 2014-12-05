0b05d8d3b63866e6d75ad274e461bf5210974726
TheoWinter <theo.winter@gmail.com>
2014-12-05 09:53:14 +0100

    basic server & client setup

--- BoardroomJS/server.js
+++ BoardroomJS/server.js
@@ -1,3 +1,42 @@
 /**
  * Created by theowinter on 05/12/14.
  */
+
+var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+var app = express();
+app.use(allowCrossDomain);
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: '2234567890QWERTY'}));
+app.use(app.router);
+
+
+//Configure the server to use the client-folder:
+//app.set('client', __dirname+'/client');
+app.configure(function() {
+    app.use(express.static(__dirname + '/client'));
+    app.use(express.static(__dirname + '/bower_components'));
+});
+
+app.use('/', express.static(__dirname + '/client/'));
+
+//socket:
+io = io.listen(app.listen(process.env.PORT || 4730));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { action: 'connected' });
+});
+
+io.sockets.on('disconnect', function (socket) {
+    socket.emit('message', { action: 'disconnect' });
+});
